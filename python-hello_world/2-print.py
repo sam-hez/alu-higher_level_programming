c3b448ae159f7e82d1dd57b62f56345f461c6257
sam-hez <h.epodoi@alustudent.com>
2026-02-27 06:17:09 -0800

    Fix 2-print.pu

--- python-hello_world/2-print.py
+++ python-hello_world/2-print.py
@@ -1,7 +1,2 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-print("\"Programming is like building a multilingual puzzle") 
-
-=======
-print("\"Programming is like building a multilingual puzzle")
->>>>>>> bb932a37684075b394eae7411e466595d48feafb
+print('"Programming is like building a multilingual puzzle')
