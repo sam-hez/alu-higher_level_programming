f7c740109b163dca443861d23f285d9ee72e77fd
sam-hez <h.epodoi@alustudent.com>
2026-06-14 07:33:12 +0200

    1. 3 languages

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,7 @@
+#!/usr/local/bin/node
+
+const firstLanguage = "C is fun";
+const secondLanguage = "Python is cool";
+const thirdLanguage = "JavaScript is amazing";
+
+console.log(firstLanguage,"\n",secondLanguage,"\n",thirdLanguage)
