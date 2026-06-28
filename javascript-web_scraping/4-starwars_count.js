39fe2e2ee8e1efdd98a061a8a920c8838d23dc18
sam-hez <h.epodoi@alustudent.com>
2026-06-28 12:25:37 +0200

    Fix applied for task 4

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -6,6 +6,30 @@ const request = require('request');
 const url = process.argv[2];
 const wedgeId = '18';
 
+function isWedgeCharacter (character) {
+  if (!character) {
+    return false;
+  }
+
+  if (typeof character === 'object') {
+    if (typeof character.url === 'string') {
+      character = character.url;
+    } else if (typeof character.id === 'number' || typeof character.id === 'string') {
+      return String(character.id) === wedgeId;
+    } else if (typeof character.name === 'string') {
+      return character.name === 'Wedge Antilles';
+    }
+  }
+
+  const value = String(character);
+  if (value === wedgeId || value === `people/${wedgeId}` || value === `/people/${wedgeId}` || value === `/people/${wedgeId}/`) {
+    return true;
+  }
+
+  const match = value.match(/\/people\/(\d+)\/?$/);
+  return match && match[1] === wedgeId;
+}
+
 request(url, (err, response, body) => {
   if (err) {
     console.log(err);
@@ -13,6 +37,6 @@ request(url, (err, response, body) => {
   }
 
   const data = JSON.parse(body);
-  const count = data.results.filter((film) => film.characters.includes(`https://swapi-api.alx-tools.com/api/people/${wedgeId}/`)).length;
+  const count = data.results.filter((film) => film.characters.some(isWedgeCharacter)).length;
   console.log(count);
 });
